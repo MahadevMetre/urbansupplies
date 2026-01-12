522e1515803f96a512427b2c3c6e6eac8a6f9bf6
Mahadev Metre <mahadev.metre666@gmail.com>
2026-01-12 17:21:25 +0530

    Implement FAQ accordion for question toggling
    Added FAQ accordion functionality to toggle active state.

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ setInterval(() => {
   index = (index + 1) % totalSlides;
   showSlide(index);
 }, 3000);
+
+// FAQ Accordion
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+    const isOpen = item.classList.contains("active");
+
+    faqItems.forEach((i) => i.classList.remove("active"));
+
+    if (!isOpen) {
+      item.classList.add("active");
+    }
+  });
+});
